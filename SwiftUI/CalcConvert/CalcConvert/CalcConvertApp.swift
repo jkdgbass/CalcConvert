3041b53d3e348d97d3d11a7ffac46ee1a5760574
dhkim <dhkim4@enliple.com>
2022-06-27 16:56:25 +0900

    SwiftUI 최초 버전
    swiftUI 최초 버전
    asset 추가

---
+++ SwiftUI/CalcConvert/CalcConvert/CalcConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcConvertApp.swift
+//  CalcConvert
+//
+//  Created by 김도형 on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct CalcConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
